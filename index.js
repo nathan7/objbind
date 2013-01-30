b53c610922235f4dab3a1a9a303d6608386050f8
Nathan Zadoks <nathan@nathan7.eu>
2013-01-30 02:30:46 +0100

    fix erroneous return left from Object.keys().forEach version

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function objBind() {
     , newObj = Object.create(obj)
 
   for (var key in obj) {
-    if (typeof obj[key] !== 'function') return
+    if (typeof obj[key] !== 'function') continue
     newObj[key] = Function.prototype.bind.apply(obj[key], [obj].concat(args))
   }
 
